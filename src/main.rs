323a6b15eee5ba85eb142399b3466780ffc40d85
TAKEDA-Takashi <takeda.takashi0308@gmail.com>
2021-02-24 16:12:27 +0900

    Call S3 Select

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,26 @@ use tokio::process::Command;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let output = Command::new("aws")
-        .args(&["sts", "get-caller-identity"])
+        .args(&[
+            "s3api",
+            "select-object-content",
+            "--bucket=testdata-xxxx",
+            "--key=test_data.json",
+            "--input-serialization",
+            r#"{"JSON":{"Type":"LINES"}}"#,
+            "--output-serialization",
+            r#"{"JSON":{"RecordDelimiter":"\n"}}"#,
+            "--expression",
+            "SELECT * FROM s3object s LIMIT 5",
+            "--expression-type=SQL",
+            "output.json",
+        ])
         .output()
         .await?;
     println!("{:?}", output);
+
+    let contents = tokio::fs::read("output.json").await?;
+    println!("{:?}", String::from_utf8(contents));
 
     Ok(())
 }
